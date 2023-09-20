3dd6b3b3440f333a95431e518b3c6ae7c9c4d854
Martin <martincohen98@gmail.com>
2023-09-19 21:37:12 -0300

    Implementada solucion trivial

--- solver.py
+++ solver.py
@@ -25,6 +25,9 @@ class Sucursal:
     def getY(self) -> float:
         return self.y
     
+    def getNumero(self) -> int:
+        return self.numero
+    
 
 class Modelo:
 
@@ -44,9 +47,45 @@ class Modelo:
     def agregarCoordenadasSucursal(self, numero, x, y):
         self.sucursales[numero].setCoordenadas(x, y)
 
+    def getCantidadSucursales(self) -> int:
+        return len(self.sucursales)
+    
+    def getSucursal(self, numero) -> Sucursal:
+        return self.sucursales[numero]
+
     def printSucursal(self, numero):
         sucursal = self.sucursales[numero]
         print(str(numero) + ": Demanda: " + str(sucursal.getDemanda()) + " Coordinadas: " + str(sucursal.getX()) + ", " + str(sucursal.getY()))
+
+
+class SolucionTrivial:
+
+    modeloOrdenado = []
+    modelo = None
+
+    def __init__(self, modelo: Modelo) -> None:
+        
+        dinero = 0
+
+        for i in range(0, modelo.getCantidadSucursales()):
+            sucursal = modelo.getSucursal(i + 1)
+
+            dinero = dinero + sucursal.getDemanda()
+
+            if(dinero < 0):
+                print("Modelo trivial imposible.")
+                return
+            
+            self.modeloOrdenado.append(sucursal.getNumero())
+
+
+    def imprimirSolucion(self) -> str:
+        texto = ""
+        for numero in self.modeloOrdenado:
+            texto = texto + str(numero) + ' '
+        
+
+        return texto[:-1]
 
 
 f = open("primer_problema.txt", "r")
@@ -78,6 +117,6 @@ for i in range(0, dimension):
     coordenada = f.readline().split(' ')
     modelo.agregarCoordenadasSucursal(int(coordenada[0]), float(coordenada[1]), float(coordenada[2]))
 
-modelo.printSucursal(1)
-modelo.printSucursal(89)
-modelo.printSucursal(5)
+solucionTrivial = SolucionTrivial(modelo)
+
+print(solucionTrivial.imprimirSolucion())
